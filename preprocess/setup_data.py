09af64d93f43bb3f898f87acedfc421de2110a7d
Eric Schulman <ericschulman@utexas.edu>
2019-08-06 20:10:52 -0500

    added sizes.txt

--- preprocess/setup_data.py
+++ preprocess/setup_data.py
@@ -25,31 +25,28 @@ def parse_arguments(argv):
 
 
 def preprocess(img):
+	"""for now, break each sentence down further into individual words 
+	using vertical white spaces"""
 	lines = crop_sentences(img)
 	words = []
 	for line in lines:
-		print('yo')
 		raw_pix = np.array(line)
 		start_index = 0
 		while start_index < raw_pix.shape[1] :
 			jump  = np.argmax( raw_pix[:,start_index:].min(axis=0) )
-			print('start', start_index, jump, raw_pix.shape[1])
 			if  jump > 0:
-				print('update1', start_index, start_index + jump, raw_pix.shape[1])
 				pix = raw_pix[:,start_index:start_index + jump]
 				crop_img = Image.fromarray(pix)
 				words.append(crop_img)
 				start_index = jump + start_index
 			else:
 				start_index = start_index +1
-				print('update2', start_index, start_index + jump, raw_pix.shape[1])
-		#while we are before the end of the line
+
 	return words
 
 
 def crop_sentences(img):
-	"""cut down on the size of the images by making smaller 100x100 chunks of 
-	the panagram"""
+	"""cut down on the size of the images by making smaller sentences size chunks"""
 	raw_pix = np.array(img)
 	preprocess_im = []
 	#divide the panagram into lines
@@ -70,6 +67,18 @@ def crop_sentences(img):
 
 	return preprocess_im
 
+
+def crop(img,size):
+	"""add necessary white space and crop image"""
+	raw_pix = np.array(img)
+	crop_img = np.ones((size,size))*255
+	
+	bottom = np.min((size,raw_pix.shape[0]))
+	right = np.min((size,raw_pix.shape[1]))
+
+	crop_img[:bottom,:right] = raw_pix[:bottom,:right]
+	final_img = Image.fromarray(crop_img)
+	return final_img
 
 
 def main(args):
@@ -104,8 +113,20 @@ def main(args):
 
 			processed_imgs = preprocess(img)
 			for p_ind in range(len(processed_imgs)):
-				new_fpath = os.path.join(fam_path, 'fam' + str(family) + '_' + number + str(p_ind) +'.png')
-				processed_imgs[p_ind].save(new_fpath, 'png')
+				img_name = 'fam' + str(family) + '_' + number + str(p_ind)
+
+				#update sizes.txt
+				sizes = open(fam_path+'/sizes.txt','a')
+
+				sizes.write(img_name + ',' + str(processed_imgs[p_ind].size[0]) 
+					+ ',' + str(processed_imgs[p_ind].size[1]) + '\n')
+				sizes.close()
+
+				#resize/crop and save
+				final_img = crop(processed_imgs[p_ind],100)
+				if final_img.mode != 'RGB':
+					final_img = final_img.convert('RGB')
+				final_img.save(fam_path + '/' + img_name + '.png','png')
 
 
 if __name__ == '__main__':
